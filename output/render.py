5d63c68393469183bed91ecbad8e3c3b0ab22a31
Eyad <eyad.azim@gmail.com>
2023-07-21 15:32:29 +0400

    Finished up plotting with appropriate spacing for the base values text box on the right

--- output/render.py
+++ output/render.py
@@ -2,6 +2,7 @@ import os
 from jinja2 import Environment, FileSystemLoader
 import pandas as pd
 import matplotlib.pyplot as plt
+import textwrap
 
 def render_results_html(dataset_info: dict, scores: list[dict], filename: str = None):
     """
@@ -55,17 +56,30 @@ def plot_parametric_graphs(stats: pd.DataFrame, results: dict, target: str, dire
     os.makedirs(save_dir,exist_ok=True)
     for column, vals in results.items():
         x, y = vals
-        plt.figure(figsize=(8, 6))
+
+         # Create a wider figure to accommodate the graph and text box side by side
+        plt.figure(figsize=(12, 6))
+
+        # Plot the graph on the left side
+        plt.subplot(1, 2, 1)  # 1 row, 2 columns, first subplot
         plt.plot(x, y)
-        plt.title(f"Parametric Study - {column} x {target}")
+        plt.title(f"Parametric Study - {textwrap.fill(column + ' x ' + target, width=30)}")
         plt.xlabel(column)
         plt.ylabel(target)
 
-        # # Add a box of values
-        # box_text = "\n".join([f"{row.index}: {row.value}" for row in stats["Mean"]])
-        # plt.text(0.05, 0.95, box_text, transform=plt.gca().transAxes,
-        #         bbox=dict(facecolor='white', alpha=0.8))
 
+        # Add a box of values on the right side
+        plt.subplot(1, 2, 2)  # 1 row, 2 columns, second subplot
+        box_text = "Base Values:\n" + "\n\n".join([f"{textwrap.fill(str(index) + ': ' + str(row.Mean), width=40)}" for index, row in stats.iterrows()])
+        plt.text(0.1, 0.5, box_text, transform=plt.gca().transAxes,
+                bbox=dict(facecolor='white', alpha=0.8), fontsize=15, va='center')
+
+        # Remove axes from the second subplot
+        ax = plt.gca()
+        ax.axis('off')
+
+        # Adjust spacing between subplots to avoid overlapping
+        plt.tight_layout()
 
         plt.savefig(os.path.join(save_dir, f"{column}.png"))
         plt.close()
