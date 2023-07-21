b0be469902383436b4826f4731f164f2f6b6ae46
Eyad <eyad.azim@gmail.com>
2023-07-21 16:12:25 +0400

    Updated plotting to support multi-target problems with some minor updates to fix bugs and improve plots

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ models = [regression.GaussianProcessRegressor(),
 dataset_dir = "data/"
 
 def main():
+    # Test with 1 target
     dataset = pd.read_excel(dataset_dir + dataset_info['path'])
     dataset_info['size'] = len(dataset)
 
@@ -52,11 +53,42 @@ def main():
 
     scores.sort(key=lambda x: x['mse'])
 
-    render.render_results_html(dataset_info, scores, f"results/{dataset_info['name']}_results.html")
+    render.render_results_html(dataset_info, scores)
     
     stats, results = utils.regression_parametric_study(top_model, dataset, dataset_info['target'])
 
-    render.plot_parametric_graphs(stats, results, dataset_info['target'])
+    render.plot_parametric_graphs(stats, results, dataset_info['target'], 'results')
+
+def two_target():
+    # Test with 2 targets
+    dataset_info['target'] = ['Concrete compressive strength(MPa, megapascals) ', 'Age (day)']
+    dataset = pd.read_excel(dataset_dir + dataset_info['path'])
+    dataset_info['size'] = len(dataset)
+
+    data = utils.load_Xy(dataset, dataset_info['target'], dataset_info['split'])
+
+    # Using a temporary variable to hold the best performing model
+    # we'll define the best performing model by the highest r2
+    top_model = None
+    best_r2 = None
+
+    scores=[]
+    for model in models:
+        score = utils.regression_train_and_test(model, *data)
+        # Check for best R2 score
+        if (not best_r2) or (score['r2'] > best_r2):
+            top_model = model
+            best_r2 = score['r2']
+        scores.append(score)
+
+    scores.sort(key=lambda x: x['mse'])
+
+    render.render_results_html(dataset_info, scores, f"results/twoTargets")
+    
+    stats, results = utils.regression_parametric_study(top_model, dataset, dataset_info['target'])
+
+    render.plot_parametric_graphs(stats, results, dataset_info['target'], "results/twoTargets")
 
 if __name__ == "__main__":
     main()
+    two_target()
--- output/render.py
+++ output/render.py
@@ -3,8 +3,9 @@ from jinja2 import Environment, FileSystemLoader
 import pandas as pd
 import matplotlib.pyplot as plt
 import textwrap
+import numpy as np
 
-def render_results_html(dataset_info: dict, scores: list[dict], filename: str = None):
+def render_results_html(dataset_info: dict, scores: list[dict], directory: str = None):
     """
     Function that uses jinja and a saved template to create an HTML page with all the results of this experiment.
     Expects a dataset_info dictionary and a list of scores, each with the following structures:
@@ -39,8 +40,9 @@ def render_results_html(dataset_info: dict, scores: list[dict], filename: str = 
         'scores': scores
     }
 
-    if filename:
-        results_filename = filename
+    if directory:
+        os.makedirs(directory, exist_ok=True)
+        results_filename = os.path.join(directory, f'{dataset_info["name"]}_results.html')
     else:
         os.makedirs('results', exist_ok=True)
         results_filename = f'results/{dataset_info["name"]}_results.html'
@@ -51,36 +53,44 @@ def render_results_html(dataset_info: dict, scores: list[dict], filename: str = 
     
     return results_filename
 
-def plot_parametric_graphs(stats: pd.DataFrame, results: dict, target: str, directory: str = ""):
+def plot_helper(x: np.ndarray, y: np.ndarray, column : str, target: str, stats: pd.DataFrame, save_dir: str):
+    # Create a wider figure to accommodate the graph and text box side by side
+    plt.figure(figsize=(12, 6))
+
+    # Plot the graph on the left side
+    plt.subplot(1, 2, 1)  # 1 row, 2 columns, first subplot
+    plt.plot(x, y)
+    plt.title(f"Parametric Study - {textwrap.fill(target + ' x ' + column, width=30)}")
+    plt.xlabel(column)
+    plt.ylabel(target)
+
+
+    # Add a box of values on the right side
+    plt.subplot(1, 2, 2)  # 1 row, 2 columns, second subplot
+    box_text = "Base Values:\n" + "\n\n".join([f"{textwrap.fill(str(index) + ': ' + str(row.Mean), width=40)}" for index, row in stats.iterrows()])
+    plt.text(0.1, 0.5, box_text, transform=plt.gca().transAxes,
+            bbox=dict(facecolor='white', alpha=0.8), fontsize=15, va='center')
+
+    # Remove axes from the second subplot
+    ax = plt.gca()
+    ax.axis('off')
+
+    # Adjust spacing between subplots to avoid overlapping
+    plt.tight_layout()
+
+    plt.savefig(os.path.join(save_dir, f"{target}_{column}.png"))
+    plt.close()
+
+def plot_parametric_graphs(stats: pd.DataFrame, results: dict, target: str | list[str], directory: str = ""):
     save_dir = os.path.join(directory, "parametric")
     os.makedirs(save_dir,exist_ok=True)
     for column, vals in results.items():
         x, y = vals
 
-         # Create a wider figure to accommodate the graph and text box side by side
-        plt.figure(figsize=(12, 6))
+        if type(target) == str:
+            plot_helper(x, y, column, target, stats, save_dir)
+        else:
+            for i, targ in enumerate(target):
+                plot_helper(x, [val[i] for val in y], column, targ, stats, save_dir)
 
-        # Plot the graph on the left side
-        plt.subplot(1, 2, 1)  # 1 row, 2 columns, first subplot
-        plt.plot(x, y)
-        plt.title(f"Parametric Study - {textwrap.fill(column + ' x ' + target, width=30)}")
-        plt.xlabel(column)
-        plt.ylabel(target)
-
-
-        # Add a box of values on the right side
-        plt.subplot(1, 2, 2)  # 1 row, 2 columns, second subplot
-        box_text = "Base Values:\n" + "\n\n".join([f"{textwrap.fill(str(index) + ': ' + str(row.Mean), width=40)}" for index, row in stats.iterrows()])
-        plt.text(0.1, 0.5, box_text, transform=plt.gca().transAxes,
-                bbox=dict(facecolor='white', alpha=0.8), fontsize=15, va='center')
-
-        # Remove axes from the second subplot
-        ax = plt.gca()
-        ax.axis('off')
-
-        # Adjust spacing between subplots to avoid overlapping
-        plt.tight_layout()
-
-        plt.savefig(os.path.join(save_dir, f"{column}.png"))
-        plt.close()
     print("Parametric plots saved in directory: " + save_dir)
